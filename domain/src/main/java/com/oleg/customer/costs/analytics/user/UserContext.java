0ad1983540bbe99e98e7e9192f157d05508f63c5
o.chukhlian <oleg.chukhlian@pitchbook.com>
2025-05-14 18:11:13 +0300

    replace record with class.

--- domain/src/main/java/com/oleg/customer/costs/analytics/user/UserContext.java
+++ domain/src/main/java/com/oleg/customer/costs/analytics/user/UserContext.java
@@ -1,4 +1,20 @@
 package com.oleg.customer.costs.analytics.user;
 
-public record UserContext(int id, String username) {
+public class UserContext {
+
+    private final int id;
+    private final String username;
+
+    public UserContext(int id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    public int id() {
+        return id;
+    }
+
+    public String username() {
+        return username;
+    }
 }
